de2b49eaa22c2fe4b6c63792ee9a6c3de2e716ae
Adrenaline <94690246+08harshit@users.noreply.github.com>
2024-04-02 18:08:18 +0530

    init (frontend:complete) initialized frontend

---
+++ weather/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import WeatherApp from './components/weatherApp';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
